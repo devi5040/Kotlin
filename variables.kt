727d7c00b4700b47709f236dd9353af020c03877
devi5040 <dpraidola@gmail.com>
2022-10-09 13:18:31 +0530

    Program for introduce variables in Kotlin

---
+++ variables.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var x: Int = 10 //The var value can be reassigned or changed
+    x = 3
+    val y: Int =10 //The val value cannot be changed
+    //y = 9 => It will give an error
+}
